63d40daf27e69a644197d826e39bbfb47186d098
Nagibator20 <Ja_mayka@mail.ru>
2024-03-02 19:26:49 +0200

    delete old luasnip exemple

--- plugins/core.lua
+++ plugins/core.lua
@@ -71,7 +71,7 @@ return {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
       -- require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- -- add more custom luasnip configuration such as filetype extend or custom snippets
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
 
@@ -79,15 +79,6 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/plugins/snippets" } } -- load snippets paths
     end,
   },
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
